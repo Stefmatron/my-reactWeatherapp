fd19fd4440b97e30a4e361f501e439037b02f6dd
Stefanie <131598782+Stefmatron@users.noreply.github.com>
2024-02-18 10:37:53 +0100

    added the structure

---
+++ src/SearchEngine.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+export default function SearchEngine() {
+  let [city, setCity] = useState("");
+  let [search, setSearch] = useState("");
+  let temperature = 23;
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (city !== "") {
+      setSearch(`It is ${temperature}°C in ${city}`);
+    } else {
+      setSearch("");
+    }
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          placeholder="Search for a city"
+          onChange={updateCity}
+        />
+        <input type="submit" value="Search" />
+      </form>
+      <div className="searchResult">{search} </div>
+    </>
+  );
+}
